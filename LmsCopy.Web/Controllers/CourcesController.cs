1b5fa66fdb289fb310c8ff36ac84610edd806a65
Bogomaz Dmitry <d.bogomaz@itransition.com>
2022-11-27 19:19:59 +0300

    scafold project

---
+++ LmsCopy.Web/Controllers/CourcesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LmsCopy.Web.Controllers;
+
+public class CourcesController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
